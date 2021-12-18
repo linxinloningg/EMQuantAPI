200750a90ee0318394dca20988770846cb934ebf
linxinloningg <7377734+linxinloningg@user.noreply.gitee.com>
2021-12-19 00:03:42 +0800

    注册接口文件2

--- manualactive.py
+++ manualactive.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
-__author__ = 'Administrator'
+
+__author__ = 'linxinloningg'
 
 from EmQuantAPI import *
 import platform
 
-#手动激活范例(单独使用)
-#获取当前安装版本为x86还是x64
-#编辑日期：2018-09-29
+# 手动激活范例(单独使用)
+# 获取当前安装版本为x86还是x64
+# 编辑日期：2018-09-29
 data = platform.architecture()
 
 if data[0] == "64bit":
@@ -29,4 +30,4 @@ else:
 
 data = c.manualactivate("13286515438", "qQ1602368639", "email=1602368639@qq.com")
 if data.ErrorCode != 0:
-    print ("manualactivate failed, ", data.ErrorMsg)
+    print("manualactivate failed, ", data.ErrorMsg)
